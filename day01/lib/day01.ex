d67ece464ebc6d820aa4d82e802b28a2635fda37
Bill Peregoy <bill@peregoy.org>
2024-12-01 21:18:59 -0500

    Advent 2024 - Day 01

---
+++ day01/lib/day01.ex
@@ -0,0 +1,65 @@
+defmodule Day01 do
+  def part_01(data) do
+    two_lists =
+      data
+      |> String.split("\n")
+      |> Enum.map(&String.split(&1, " ", trim: true))
+      |> Enum.reject(&Enum.empty?/1)
+
+    left_list =
+      Enum.map(two_lists, &Enum.at(&1, 0))
+      |> Enum.map(fn str ->
+        {num, ""} = Integer.parse(str)
+        num
+      end)
+      |> Enum.sort()
+
+    right_list =
+      Enum.map(two_lists, &Enum.at(&1, 1))
+      |> Enum.map(fn str ->
+        {num, ""} = Integer.parse(str)
+        num
+      end)
+      |> Enum.sort()
+
+    left_list
+    |> Enum.zip(right_list)
+    |> Enum.map(fn {left, right} -> abs(right - left) end)
+    |> Enum.reduce(fn val, acc -> acc + val end)
+  end
+
+  def part_02(data) do
+    two_lists =
+      data
+      |> String.split("\n")
+      |> Enum.map(&String.split(&1, " ", trim: true))
+      |> Enum.reject(&Enum.empty?/1)
+
+    left_list =
+      Enum.map(two_lists, &Enum.at(&1, 0))
+      |> Enum.map(fn str ->
+        {num, ""} = Integer.parse(str)
+        num
+      end)
+      |> Enum.sort()
+
+    right_list =
+      Enum.map(two_lists, &Enum.at(&1, 1))
+      |> Enum.map(fn str ->
+        {num, ""} = Integer.parse(str)
+        num
+      end)
+      |> Enum.sort()
+
+    left_list
+    |> Enum.zip(right_list)
+    |> Enum.map(fn {left, _right} ->
+      count_in_right =
+        Enum.filter(right_list, &(&1 == left))
+        |> Enum.count()
+
+      count_in_right * left
+    end)
+    |> Enum.reduce(fn num, acc -> acc + num end)
+  end
+end
